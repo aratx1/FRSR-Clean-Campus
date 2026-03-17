385e5cdddb7e0fa7e51d8260ce6e99c9d1cafd7c
arantx1 <look4mydreams@hotmail.com>
2026-03-17 15:17:33 -0300

    feat: hide dropdown

--- content.js
+++ content.js
@@ -1,32 +1,55 @@
-
 const materiasAOcultar = [
-    "Materia1",
+	"Materia1",
     "Materia2",
     "Materia3",
     "Materia4",
     "Materia5"
-
 ];
 
+function limpiarCampusDropdown() {
+	//contenedores del dropdown
+    const tarjetasDropdown = document.getElementById('dropdownmain-navigation0');
+    if (!tarjetasDropdown) return;
+
+    const cursoItems = tarjetasDropdown.querySelectorAll('li > a'); 
+	
+    cursoItems.forEach(item => {
+        const cursoNombre = item.textContent.trim().toLowerCase();
+		//checkear si los items estan en la lista
+        const debeOcultarse = materiasAOcultar.some(hideTarget => cursoNombre.includes(hideTarget));
+        
+		// ocultar el padre tambien
+        if (debeOcultarse) {
+            item.parentElement.style.display = 'none'; 
+        }
+    });
+}
+
 function limpiarCampus() {
-    // contenedores
+    // contenedores de las tarjetas
     const tarjetas = document.querySelectorAll('.coursebox.hover.covtiles');
 
     tarjetas.forEach(tarjeta => {
 
         const textoTarjeta = tarjeta.innerText;
-
+        // verificar texto de las tarjetas
         const debeOcultarse = materiasAOcultar.some(nombre => 
             textoTarjeta.toLowerCase().includes(nombre.toLowerCase())
         );
-
+        // ocultar tarjeta
         if (debeOcultarse) {
             tarjeta.style.display = 'none';
         }
     });
 }
 
-limpiarCampus();
 
-const observer = new MutationObserver(limpiarCampus);
+limpiarCampus();
+limpiarCampusDropdown();
+
+const observer = new MutationObserver(() => {
+    limpiarCampus();
+	limpiarCampusDropdown();
+});
+
 observer.observe(document.body, { childList: true, subtree: true });
